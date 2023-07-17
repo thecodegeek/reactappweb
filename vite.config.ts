4b7ccdb0dc81187468c515a1d5b164c4aad315de
thecodegeek <110385459+thecodegeek@users.noreply.github.com>
2023-07-17 15:36:15 +0200

    deleted old stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {outDir: '/Users/nicolacassagovilla/Documents/Informatik/React-JS/thecodegeek.github.io', emptyOutDir: true}
+  }
+)
